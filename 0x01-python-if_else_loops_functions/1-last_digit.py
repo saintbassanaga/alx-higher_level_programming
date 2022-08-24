6e1074c04c4f2b6277f5ea40125618d7d0c0684c
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-08-24 04:32:13 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,12 +13,10 @@ elif last_digit > 5:
     text = " greather than 5"
 else:
     text = " 0"
-=======
 if last_digit == 0:
     text = " 0"
 elif last_digit > 5:
     text = " greater than 5"
 else:
     text = " less than 6 and not 0"
->>>>>>> 6f2b21ff6cab5af28771d14081faf1e096f7c4ec
 print("Last digit of {} is {} and is{}".format(number, last_digit, text))
