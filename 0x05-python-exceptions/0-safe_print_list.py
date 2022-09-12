8e7f462522ce96db81c2d5581dac8d25edd68c2e
saintbassanaga <saintbassanaga01@gmail.com>
2022-09-12 13:55:33 -0700

    Together

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             total += 1
-        except IndexError:
+        except (IndexError):
             break
     print("")
     return (total)
