d642e5919d6709d091297977e580f54a917dccc3
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-09-20 23:11:01 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(H=[]):
-    H += ["Holberton"]
-    return (", ".join(H))
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "BestSchool" + ", BestSchool" * count[0]
