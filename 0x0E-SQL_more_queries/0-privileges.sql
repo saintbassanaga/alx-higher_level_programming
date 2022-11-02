c47e57b284a5857279d5b44256c9e986cbf7200f
saintbassanaga <saintbassanaga01@gmail.com>
2022-11-02 13:44:23 -0700

    Granted privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- List all privileges of the MySQL users user_0d_1 and user_0d_2
 -- On your server in localhost
-
-SHOW GRANTS;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
