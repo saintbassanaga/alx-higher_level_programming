8a52cb885c79198dd1a120fa8cd5c7d3e1ea9401
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-12-17 20:19:10 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL -H "origin: HolbertonSchool" -X PUT 0.0.0.0:5000/catch_me_3
